a3157da424945e4f077fff2588de254df81198d2
isabellaeas <101370656+isabellaeas@users.noreply.github.com>
2022-06-14 12:07:27 +1200

    Created skeleton for 00_base_V1

---
+++ 00_base_V1.py
@@ -0,0 +1,55 @@
+"""
+Program to calculate cost for recipe per serving
+V1 - Set up skeleton of program, test functions
+"""
+
+
+# Set up functions
+
+# Function to check if recipe name is blank
+def not_blank():
+    print("placeholder")
+
+
+# Function to check for integers. Will loop until integer is input
+def int_checker():
+    print("placeholder")
+
+
+# Function to calculate cost per amount
+def cost_calculator():
+    print("placeholder")
+
+
+# Set up lists and constants
+ingredient_lst = []
+amount_needed_lst = []
+amount_purchased_lst = []
+price_lst = []
+
+# Set up main
+if __name__ == "__main__":
+    # Set up variables in main routine
+
+    # Ask users for input
+    print("****RECIPE COST CALCULATOR****")
+    print()
+    print("Welcome to the recipe cost calculator")
+
+    # Ask user for recipe name
+    # Check not blank
+
+    # Ask user for serving size
+    # Int checker
+
+    # Ask user for number of ingredients
+    # Int checker
+
+    # Loop for number of ingredients
+
+    # Ask for ingredient
+    # Ask for amount needed
+    # Ask for amount purchased
+    # Ask for price of amount purchased
+
+    # Output information

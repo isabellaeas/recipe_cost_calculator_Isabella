96d7824e945c9e27181fd46dd1a48f6809dc8f5f
isabellaeas <101370656+isabellaeas@users.noreply.github.com>
2022-06-15 14:01:14 +1200

    Added "error_message" parameter

---
+++ 01_not_blank_V2.py
@@ -0,0 +1,23 @@
+"""
+Component 1 - not blank function
+V1 -  Checking to see that function is called. Returns a basic error message
+"""
+
+
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        answer = input(question)
+
+        if answer != "":
+            return answer
+        else:
+            print(error_message)
+
+
+# Main
+# Check that the not blank function is called
+recipe_name = not_blank("Recipe name:",
+                        "sorry this can't be blank, "
+                        "please enter you recipe's name")
